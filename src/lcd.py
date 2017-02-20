a2bfc92a6c76509e9a3c0943d12c49e85b6bae95
David "Fires" Stein <info@davidstein.cz>
2017-02-20 21:10:48 +0100

    Added first version of External Call

--- src/lcd.py
+++ src/lcd.py
@@ -24,13 +24,63 @@
 
 import lcddriver
 from time import *
+from optparse import OptionParser
 
-lcd = lcddriver.lcd(0x27)
+#parse arguments
+parser = OptionParser()
+#parse values
+parser.add_option("-a", "--address", action="store", dest="address",
+                  help="I2C Adress")
+                  
+parser.add_option("-p", "--persistent",
+                  action="store_true", dest="persistent", default=False,
+                  help="-p = do not clear display before new write")
+                  
+parser.add_option("--line1",
+                  action="store", dest="line1",
+                  help="text to line 1")
 
-lcd.backlightOn()
-lcd.lcd_display_string("I2C LCD ONION.IO", 1)
-lcd.lcd_display_string(" -davidstein.cz-", 2)
-sleep(1)
-lcd.backlightOff()
-sleep(1)
-lcd.backlightOn()
+parser.add_option("--line2",
+                  action="store", dest="line2",
+                  help="text to line 2")
+                  
+parser.add_option("--line3",
+                  action="store", dest="line3",
+                  help="text to line 3")
+                  
+parser.add_option("--line4",
+                  action="store", dest="line4",
+                  help="text to line 4")              
+                  
+parser.add_option("--backlight-off",
+                  action="store_true", dest="backlightoff", default=False,
+                  help="Turn off backlight")    
+
+#parse arguments to values
+(options, args) = parser.parse_args()
+
+#process argument - address
+#lcd = lcddriver.lcd(options.address)
+lcd = lcddriver.lcd(int(options.address,0))
+
+#process argument - persistent
+if not options.persistent:
+    lcd.lcd_clear()
+    
+#process argument - backlight
+if options.backlightoff:
+    lcd.backlightOff()
+else:
+    lcd.backlightOn()
+    
+#process argument line 1-4
+for num in range(1,4):  #to iterate between lines 1 to 4
+    try:
+        getattr(options, "line%d" % num)
+    except NameError:
+        print "Line %d not set" (num)
+    except NoneType:
+        print "Line %d noneType" (num)
+    else:
+        if getattr(options, "line%d" % num) is not None and len(getattr(options, "line%d" % num)) > 0:
+            lcd.lcd_display_string(getattr(options, "line%d" % num),num);
